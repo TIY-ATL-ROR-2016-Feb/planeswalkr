a3039c2dba47268726ad4d555539273ad1436043
Brit Butler <redline6561@gmail.com>
2016-04-20 11:12:21 -0400

    Don't forget to require resque/server.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require "resque/server"
+
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
